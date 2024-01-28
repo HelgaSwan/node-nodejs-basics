17ab0012d110a18f60f85f5b3b0f2a4227f9580e
HelgaSwan <helga92011@gmail.com>
2024-01-28 22:27:25 +0100

    feat: copy files to new dir, throw error

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -6,42 +6,65 @@ import path from 'node:path';
 import { error } from 'node:console';
 import { existsSync } from 'node:fs';
 
+
+import { readdir } from 'node:fs/promises';
+
+import { copyFile, constants } from 'node:fs';
+
+
 const rootFile = fileURLToPath(import.meta.url);
 const rootDir = dirname(rootFile);
 const srcDir = path.join(rootDir, 'files');
 const newDir = path.join(rootDir, 'files_copy');
 
 const copy = async () => {
-    
-  function createDir() {
-
-    fs.mkdir(newDir, {
-        recursive: true,
-    }, err => {
-        if (err) throw err('FS operation failed');
-    });
-
-  }; 
-
     try {
         const srcDirExists = existsSync(srcDir);
         if (srcDirExists === false) {
-            throw error;            
-    }
-    else {
-        const newDirExists = existsSync(newDir);
-        // console.log('Exists: ', newDirExists);
-        if (newDirExists === false) {
-            createDir();
-            console.log('New dir created');
+            throw error;  
         }
-        else throw error;
-    }
-        
+        else {
+            const newDirExists = existsSync(newDir);
+            // console.log('Exists: ', newDirExists);
+            if (newDirExists === false) {
+                console.log('New dir created');
+                
+            }
+            else throw error;
+    }      
     
     } catch (e) {
         console.error('FS operation failed');
+        return;
     }
+
+    (function createDir() {            
+
+        fs.mkdir(newDir, {
+            recursive: true,
+        }, err => {
+            if (err) throw error;            
+        });
+        // console.log("1");
+    })(); 
+
+    try {
+      const files = await readdir(srcDir);
+      for (const file of files) {
+
+    function callback(err) {
+    if (err) throw err('file exists');
+    }
+        copyFile(path.join(srcDir, file), path.join(newDir, file), callback);
+    }
+    } catch (err) {
+      console.error(err);
+    return;
+    }
+
+
+//   console.log("2");
+
     
 };
 
